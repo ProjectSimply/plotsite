8cd29dd903f5c0b1132d5ddab1995b9a75e59b5e
Matty <matty@projectsimply.com>
2020-04-17 15:25:48 +0100

    Started styling burger menu

--- wp-content/themes/plot-custom/templates/parts/burger-menu.php
+++ wp-content/themes/plot-custom/templates/parts/burger-menu.php
@@ -1,5 +1,15 @@
 <section id="burgerMenu" class="JS--burgerMenu">
 
-		<?php wp_nav_menu( array(  'menu' => 'Main Menu', 'menu_class' => 'burgerMenu' ) ) ?>
+	<div class="burgerMenu__innerWrap">
+
+		<?php wp_nav_menu( array(  'menu' => 'Main Menu', 'menu_class' => 'burgerMenu__list' ) ) ?>
+
+		<div class="burgerMenu__cta">
+			<a class="button burgerMenu__ctaLink" href="<?= get_field('call_to_action_link', 'options') ?>">
+					<?= get_field('call_to_action_text', 'options') ?>
+			</a>
+		</div>
+
+	</div>
 
 </section>
