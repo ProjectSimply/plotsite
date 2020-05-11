47f418bb9ddc7564dbef65d9e7a528281e510b86
Matty <matty@projectsimply.com>
2020-05-11 14:46:23 +0100

    Add cards styles

--- wp-content/themes/plot-custom/templates/parts/cards.php
+++ wp-content/themes/plot-custom/templates/parts/cards.php
@@ -2,7 +2,39 @@
 
     <div class="maxWidth">
 
-        
+        <div class="cards__intro">
+
+            <h2 class="cards__sectionHeading"><?= get_field('cards_section_title'); ?></h2>
+
+            <p class="cards__sectionIntro"><?= get_field('cards_section_subtext'); ?></p>
+
+        </div>
+
+        <div class="cards__grid">
+
+            <?php while ( has_sub_field('cards')) : ?>
+
+                <div class="cards__item">
+
+                    <div class="cards__svgWrap">
+
+                        <?= get_sub_field('card_svg'); ?>
+
+                    </div>
+
+                    <div class="cards__textWrap">
+
+                        <h5 class="cards__title"><?= get_sub_field('card_title') ?></h5>
+
+                        <p><?= get_sub_field('card_text'); ?></p>
+
+                    </div>
+
+                </div>
+
+            <?php endwhile; ?>
+
+        </div>
 
     </div>
 
