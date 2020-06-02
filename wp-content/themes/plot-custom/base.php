db30c79dd30e7210d65103844b1f475cced939e0
Matty <matty@projectsimply.com>
2020-06-02 08:22:12 +0100

    Set banner theme inital to "deep"

---
+++ wp-content/themes/plot-custom/base.php
@@ -0,0 +1,44 @@
+<?php plotGetTemplatePart('parts/head'); ?>
+
+<body data-current-theme="deep" <?php plotBodyClass() ?>> 
+
+    <?php if(get_field('gtm_id','option')) : ?>
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= get_field('gtm_id', 'option') ?>"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    
+    <?php endif; ?>
+
+    <?php plotGetNotifications() ?>  
+
+    <div data-plot-smooth-scroll>
+
+        <div class="siteWrap">
+
+                <?php include plotTemplatePath(); ?>
+
+        </div>
+
+    </div>
+
+    <?php plotGetTemplatePart('parts/schedule-download'); ?>
+
+    <?php plotGetTemplatePart('parts/burger-menu') ?>
+
+    <div class="siteOverlay"></div>
+
+    <?php plotGetTemplatePart('parts/modal') ?>
+    
+    <?php plotGetTemplatePart('parts/site-status-note'); ?>  
+
+    <?php plotGetTemplatePart('parts/custom-mouse') ?>
+
+    <?php plotGetTemplatePart('parts/footer-javascript'); ?>
+
+    <?php wp_footer(); ?>
+
+    <?php plotGetTemplatePart('parts/live-reload'); ?>
+    
+</body>
+
+</html>
