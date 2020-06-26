dfcdbe38d0a20e6fe39dc75d28915e6793504151
Matty <matty@projectsimply.com>
2020-06-26 08:31:55 +0100

    Add 404 page

---
+++ wp-content/themes/plot-custom/templates/pages/404.php
@@ -0,0 +1,30 @@
+<?php plotGetTemplatePart('parts/header') ?>
+
+<div data-plot-smooth-scroll-frame class="fourOhFourPage maxWidth--narrow plotPage">
+
+	<div class="fourOhFourPage__contentWrap">
+
+		<div class="fourOhFourPage__contentBox">
+
+			<h3>Sorry, the page you are looking for does not exist. This could be because of: </h3>
+			<p>Please use the navigation at the top of the page to continue your journey. Alternatively, visit our <a href="<?php bloginfo('url')?>">home page</a>.</p>		                
+
+		</div>
+
+	</div>
+
+	<div class="fourOhFourPage__asset fourOhFourPage__asset--1">
+
+		<?php plotGetTemplatePart('parts/cross-asset') ?>
+
+	</div>
+
+	<div class="fourOhFourPage__asset fourOhFourPage__asset--2">
+
+		<?php plotGetTemplatePart('parts/cross-asset') ?>
+
+	</div>
+
+</div>
+
+<?php plotGetTemplatePart('parts/footer') ?>
