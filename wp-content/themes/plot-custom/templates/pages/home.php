c91e73217ff349b9a999d709965aaa29c8523877
Matty <matty@projectsimply.com>
2020-07-15 11:15:06 +0100

    Pre homepage hero images

--- wp-content/themes/plot-custom/templates/pages/home.php
+++ wp-content/themes/plot-custom/templates/pages/home.php
@@ -9,3 +9,60 @@
 <?php plotGetTemplatePart('parts/carousel') ?>
 
 <?php plotGetTemplatePart('parts/footer') ?>
+
+
+<div id="preload">
+    <!-- Africa oye -->
+    <img src="<?= IMAGES ?>/heros/africa-oye/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/africa-oye/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/africa-oye/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- arts-and-kraft -->
+    <img src="<?= IMAGES ?>/heros/arts-and-kraft/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/arts-and-kraft/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/arts-and-kraft/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Back in the future -->
+    <img src="<?= IMAGES ?>/heros/back-in-the-future/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/back-in-the-future/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/back-in-the-future/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Deep State -->
+    <img src="<?= IMAGES ?>/heros/deep-state/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/deep-state/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/deep-state/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Gas Giant -->
+    <img src="<?= IMAGES ?>/heros/gas-giant/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/gas-giant/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Halftone Point -->
+    <img src="<?= IMAGES ?>/heros/halftone-point/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/halftone-point/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/halftone-point/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Highest Point  -->
+    <img src="<?= IMAGES ?>/heros/highest-point/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/highest-point/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/highest-point/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Mi Casa -->
+    <img src="<?= IMAGES ?>/heros/mi-casa/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/mi-casa/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/mi-casa/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- rhythm and waves -->
+    <img src="<?= IMAGES ?>/heros/rhythm-and-waves/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/rhythm-and-waves/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/rhythm-and-waves/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- Sound ways -->
+    <img src="<?= IMAGES ?>/heros/sound-ways/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/sound-ways/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/sound-ways/Right.jpg" width="1" height="1" alt="Image 01" />
+
+    <!-- The Box -->
+    <img src="<?= IMAGES ?>/heros/the-box/Screen.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/the-box/Left.jpg" width="1" height="1" alt="Image 01" />
+    <img src="<?= IMAGES ?>/heros/the-box/Right.jpg" width="1" height="1" alt="Image 01" />
+</div>
